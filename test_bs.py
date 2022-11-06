9cd5ed30a9a9433a9973a202002148e7605d7e18
olgakurg <olgakurg@gmail.com>
2022-11-06 15:25:03 +0800

    Update test_bs.py

--- test_bs.py
+++ test_bs.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
 def is_not_external(href, url):
-    if urlparse(url).hostname == urlparse(href).hostname:
+    if 'http' not in href or urlparse(url).hostname == urlparse(href).hostname:
         return True
     else:
         return False
@@ -16,19 +16,33 @@ def get_links(url):
 
     for a_tag in soup.findAll("a"):
         href = a_tag.attrs.get("href")
-        if href == "" or href is None or '//' not in href or is_not_external(href, url):
+        if href == "" or href is None or is_not_external(href, url):
             continue
         hrefs.add(href)
         
     return hrefs
 
+def urls_to_file(url, urls, path):
+    with open(path, 'a', encoding='utf-8') as file:
+        file.write(f'Для ссылки {url} получили набор ссылок: \n')
+        for u in urls:
+            file.write(f'link {u} \n')
+        
+
+
 if __name__ == '__main__':
        
     url = 'http://edu.buk.irk.ru'
+    path = 'test.txt'
+
+    print (f'Парсим {url}, Вывод результатов в файл {path}')
+
     urls = get_links(url)
+    urls_to_file(url, urls, path)
+
     for url in urls:
         new_urls = get_links(url)
-        print (new_urls)
+        urls_to_file(url, new_urls, path)
 
 
-    #TODO  - регулярка для внутренних ссылок, доделать джампы. потом - вывод в файл или в консоль
+    #TODO  - доделать джампы. потом - вывод в файл или в консоль

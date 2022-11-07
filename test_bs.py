7ca0f2b3a33da6c733896f1affc4d2dad822c2ff
olgakurg <olgakurg@gmail.com>
2022-11-07 18:12:27 +0800

    Update test_bs.py

--- test_bs.py
+++ test_bs.py
@@ -2,12 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
-def is_not_external(href, url):
-    if 'http' not in href or urlparse(url).hostname == urlparse(href).hostname:
-        return True
-    else:
-        return False
-
 def get_links(url):        
     
     page = requests.get(url)
@@ -16,33 +10,38 @@ def get_links(url):
 
     for a_tag in soup.findAll("a"):
         href = a_tag.attrs.get("href")
-        if href == "" or href is None or is_not_external(href, url):
+        if href == "" or href is None or 'http' not in href or urlparse(url).hostname == urlparse(href).hostname:
             continue
         hrefs.add(href)
+
+    if param1 == 'c':
+        print (f'\nДля ссылки {url} получили набор ссылок:')
+        for link in hrefs:
+            print(f'{link}')
+    elif param1 == 'f':
+        with open(path, 'a', encoding='utf-8') as file:
+            file.write(f'\nДля ссылки {url} получили набор ссылок:')
+            for link in hrefs:
+                file.write(f'{link} \n')
         
     return hrefs
 
-def urls_to_file(url, urls, path):
-    with open(path, 'a', encoding='utf-8') as file:
-        file.write(f'Для ссылки {url} получили набор ссылок: \n')
-        for u in urls:
-            file.write(f'link {u} \n')
-        
 
+  
 
 if __name__ == '__main__':
        
     url = 'http://edu.buk.irk.ru'
     path = 'test.txt'
 
-    print (f'Парсим {url}, Вывод результатов в файл {path}')
+    print (f'Парсим {url}')
+    param1 = input(f'Сохраняем результаты в файл {path} (f) или выводим в терминал (с)? ')
+    param2 = input (f'Для каждой полученной ссылки процедуру повторяем (у/n)? ')
 
-    urls = get_links(url)
-    urls_to_file(url, urls, path)
-
-    for url in urls:
-        new_urls = get_links(url)
-        urls_to_file(url, new_urls, path)
-
-
-    #TODO  - доделать джампы. потом - вывод в файл или в консоль
+    if param1 and param2:
+        urls = get_links(url)
+        if param2 == 'y':
+            for url in urls:
+                new_urls = get_links(url)
+    else:
+        print ('неизвестные параметры, проверьте раскладку и запустите заново')
